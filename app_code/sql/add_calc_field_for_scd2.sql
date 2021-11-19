1b803a05e0d8b92578b553dc7140ad0e12515e39
Melody Y <meloyang@amazon.com>
2021-11-19 15:17:34 +1100

    add time travel section to the notebook

--- app_code/sql/add_calc_field_for_scd2.sql
+++
@@ -1,9 +0,0 @@
-SELECT id
-, name
-, email
-, state
-, ${CURRENT_TIMESTAMP} AS valid_from
-, CAST(null AS timestamp) AS valid_to
-, 1 AS iscurrent
-, md5(concat(name,email,state)) AS checksum 
-FROM ${table_name}
